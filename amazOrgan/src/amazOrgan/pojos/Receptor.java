3a390fe248c90cc5b62d23e8fafe3ce1b5caab27
letib <letib@10.60.81.81>
2022-04-22 19:11:06 +0200

    again

--- amazOrgan/src/amazOrgan/pojos/Receptor.java
+++ amazOrgan/src/amazOrgan/pojos/Receptor.java
@@ -1,10 +1,5 @@
 package amazOrgan.pojos;
 
 public class Receptor {
-<<<<<<< HEAD
   int id ;
-=======
-  System.out.println("Hola Maria")
-
->>>>>>> branch 'master' of https://github.com/leticia-bmm/amazOrgan
 }
