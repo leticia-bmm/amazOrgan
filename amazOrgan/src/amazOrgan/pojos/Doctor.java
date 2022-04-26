65aa58a6f0f31e6a8457497796984fff262385f1
jprat <jprat@LAPTOP-8A1FF2SQ>
2022-04-26 18:59:05 +0200

    DOCTOR CREATED

--- amazOrgan/src/amazOrgan/pojos/Doctor.java
+++ amazOrgan/src/amazOrgan/pojos/Doctor.java
@@ -1,5 +1,59 @@
 package amazOrgan.pojos;
 
-public class Doctor {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Doctor implements Serializable {
+	
+	
+	private Integer Medical_ID;
+	private Integer Phone_number;
+	private String Name;
+	
+	
+	
+	public Doctor() {
+		super();
+	}
+	public Integer getMedical_ID() {
+		return Medical_ID;
+	}
+	public void setMedical_ID(Integer medical_ID) {
+		Medical_ID = medical_ID;
+	}
+	public Integer getPhone_number() {
+		return Phone_number;
+	}
+	public void setPhone_number(Integer phone_number) {
+		Phone_number = phone_number;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	@Override
+	public String toString() {
+		return "Doctor [Medical_ID=" + Medical_ID + ", Phone_number=" + Phone_number + ", Name=" + Name + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(Medical_ID, Name, Phone_number);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		return Objects.equals(Medical_ID, other.Medical_ID) && Objects.equals(Name, other.Name)
+				&& Objects.equals(Phone_number, other.Phone_number);
+	}
+	
+	
+	
 }
