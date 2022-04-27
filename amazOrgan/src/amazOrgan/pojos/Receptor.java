b53cc706df715866a28b231f54035aad9658106f
letib <letib@10.60.83.219>
2022-04-27 20:20:26 +0200

    receptor updated

--- amazOrgan/src/amazOrgan/pojos/Receptor.java
+++ amazOrgan/src/amazOrgan/pojos/Receptor.java
@@ -1,6 +1,7 @@
 package amazOrgan.pojos;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 public class Receptor implements Serializable{
@@ -14,104 +15,119 @@ public class Receptor implements Serializable{
 	  private Antigen antigen;
 	  private Antibody antibody;
 	  private Location location;
-	  private Doctor doctor;
 	  private Request request;
+	  private List <Doctor> doctors;
 	 
 	  
-	  //empty Constructor
+	//empty Constructor
 	public Receptor() {
 		super();
 	}
-	
-	//Getters and Setters
+
+	//getters setters
 	public Integer getDni() {
 		return dni;
 	}
+
 
 	public void setDni(Integer dni) {
 		this.dni = dni;
 	}
 
+
 	public Integer getDob() {
 		return dob;
 	}
+
 
 	public void setDob(Integer dob) {
 		this.dob = dob;
 	}
 
+
 	public String getStatus() {
 		return status;
 	}
+
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
+
 	public String getBlood_type() {
 		return blood_type;
 	}
+
 
 	public void setBlood_type(String blood_type) {
 		this.blood_type = blood_type;
 	}
 
+
 	public Integer getUrgency() {
 		return urgency;
 	}
+
 
 	public void setUrgency(Integer urgency) {
 		this.urgency = urgency;
 	}
 
+
 	public Antigen getAntigen() {
 		return antigen;
 	}
+
 
 	public void setAntigen(Antigen antigen) {
 		this.antigen = antigen;
 	}
 
+
 	public Antibody getAntibody() {
 		return antibody;
 	}
+
 
 	public void setAntibody(Antibody antibody) {
 		this.antibody = antibody;
 	}
 
+
 	public Location getLocation() {
 		return location;
 	}
+
 
 	public void setLocation(Location location) {
 		this.location = location;
 	}
 
-	public Doctor getDoctor() {
-		return doctor;
-	}
-
-	public void setDoctor(Doctor doctor) {
-		this.doctor = doctor;
-	}
 
 	public Request getRequest() {
 		return request;
 	}
 
+
 	public void setRequest(Request request) {
 		this.request = request;
 	}
 
-	
-	
 
-	
-	//hashcode
+	public List<Doctor> getDoctors() {
+		return doctors;
+	}
+
+
+	public void setDoctors(List<Doctor> doctors) {
+		this.doctors = doctors;
+	}
+
+	//hash code
 	@Override
 	public int hashCode() {
-		return Objects.hash(antibody, antigen, blood_type, dni, dob, doctor, location, request, status, urgency);
+		return Objects.hash(dni);
 	}
 
 	@Override
@@ -123,31 +139,16 @@ public class Receptor implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Receptor other = (Receptor) obj;
-		return Objects.equals(antibody, other.antibody) && Objects.equals(antigen, other.antigen)
-				&& Objects.equals(blood_type, other.blood_type) && Objects.equals(dni, other.dni)
-				&& Objects.equals(dob, other.dob) && Objects.equals(doctor, other.doctor)
-				&& Objects.equals(location, other.location) && Objects.equals(request, other.request)
-				&& Objects.equals(status, other.status) && Objects.equals(urgency, other.urgency);
+		return Objects.equals(dni, other.dni);
 	}
 
-	
-	//toString
+	//to string
 	@Override
 	public String toString() {
 		return "Receptor [dni=" + dni + ", dob=" + dob + ", status=" + status + ", blood_type=" + blood_type
 				+ ", urgency=" + urgency + ", antigen=" + antigen + ", antibody=" + antibody + ", location=" + location
-				+ ", doctor=" + doctor + ", request=" + request + "]";
+				+ ", request=" + request + ", doctors=" + doctors + "]";
 	}
-
-	
-
-	
-	
-	
-	  
-	  
-	  
-	  
 	  
 	
 }
