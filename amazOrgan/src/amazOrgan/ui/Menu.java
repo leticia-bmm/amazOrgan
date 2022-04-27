68b6ccfc568a4dec049ccfc34cb661c2f7e3520a
maria-ml <maria.m.labrador@gmail.com>
2022-04-27 20:28:45 +0200

    menu started

---
+++ amazOrgan/src/amazOrgan/ui/Menu.java
@@ -0,0 +1,20 @@
+package amazOrgan.ui;
+
+
+public class Menu {
+	
+	public static void main (String[] ars) {
+		
+		System.out.println("Welcome to amazOrgan!");
+		// Initialize database for JDBC
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
