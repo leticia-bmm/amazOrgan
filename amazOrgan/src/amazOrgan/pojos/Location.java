9eb589c33e38875c5793b17d9a6109ffba167bd5
maria-ml <maria.m.labrador@gmail.com>
2022-04-27 12:40:51 +0200

    Location updated

--- amazOrgan/src/amazOrgan/pojos/Location.java
+++ amazOrgan/src/amazOrgan/pojos/Location.java
@@ -3,53 +3,39 @@ package amazOrgan.pojos;
 import java.io.Serializable;
 
 public class Location implements Serializable{
-		
-		/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2724475628719553532L;
-		private Integer id;
-		private Float latitude;
-		private Float longitude;
-		/**
-		 * @return the id
-		 */
-
-public Location() {
 	
-}
+	private static final long serialVersionUID = 2724475628719553532L;
+	
+	private Integer id;
+	private Float latitude;
+	private Float longitude;
+		
+	
+	public Location() {
+	
+	}
 	
 		public Integer getId() {
 			return id;
 		}
 		
-		/**
-		 * @param id the id to set
-		 */
+		
 		public void setId(Integer id) {
 			this.id = id;
 		}
-		/**
-		 * @return the latitude
-		 */
+		
 		public Float getLatitude() {
 			return latitude;
 		}
-		/**
-		 * @param latitude the latitude to set
-		 */
+		
 		public void setLatitude(Float latitude) {
 			this.latitude = latitude;
 		}
-		/**
-		 * @return the longitude
-		 */
+		
 		public Float getLongitude() {
 			return longitude;
 		}
-		/**
-		 * @param longitude the longitude to set
-		 */
+		
 		public void setLongitude(Float longitude) {
 			this.longitude = longitude;
 		}
