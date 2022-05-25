ba0229f7d17596dd5414e98cbcccf1f63c558962
maria-ml <maria.m.labrador@gmail.com>
2022-05-25 19:47:53 +0200

    creo que es lo mismo

--- amazOrgan/src/amazOrgan/pojos/OrganList.java
+++ amazOrgan/src/amazOrgan/pojos/OrganList.java
@@ -1,5 +1,46 @@
 package amazOrgan.pojos;
 
+import java.util.List;
+import java.util.Objects;
+
 public class OrganList {
 
+	private List<Organ> organs;
+
+	public OrganList(List<Organ> organs) {
+		super();
+		this.organs = organs;
+	}
+
+	public List<Organ> getOrgans() {
+		return organs;
+	}
+
+	public void setOrgans(List<Organ> organs) {
+		this.organs = organs;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(organs);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrganList other = (OrganList) obj;
+		return Objects.equals(organs, other.organs);
+	}
+
+	@Override
+	public String toString() {
+		return "OrganList [organs=" + organs + "]";
+	}
+	
+	
 }
